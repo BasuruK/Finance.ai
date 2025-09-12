6626ea23eec165510130bb22266d5a25eab64ab0
BasuruK <www.basururoxyouall@gmail.com>
2025-09-12 21:43:37 +0530

    feat: update Azure Static Web Apps workflow and configuration for improved deployment and caching

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
   reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+    unoptimized: false
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
   },
